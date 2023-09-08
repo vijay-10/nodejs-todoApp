2d6fd239be8f7c0ea21fa479ec51fa62e7a9ad97
vijay-10 <vijayvardhansn10@gmail.com>
2023-09-08 13:50:47 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,46 @@
+import express from "express";
+import userRouter from "./routes/user.js";
+import taskRouter from "./routes/task.js";
+import { config } from "dotenv";
+import cookieParser from "cookie-parser";
+import { errorMiddleware } from "./middlewares/error.js";
+import cors from "cors";
+import { connectDB } from "./config/database.js";
+connectDB();
+
+export const app = express();
+
+
+
+app.listen(process.env.PORT, () => {
+  console.log(
+    `Server is working on port:${process.env.PORT} in ${process.env.NODE_ENV} Mode`
+  );
+});
+
+
+config({
+  path: "./.env",
+});
+
+// Using Middlewares
+app.use(express.json());
+app.use(cookieParser());
+app.use(
+  cors({
+    origin: [process.env.FRONTEND_URL],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
+
+// Using routes
+app.use("/api/v1/users", userRouter);
+app.use("/api/v1/task", taskRouter);
+
+app.get("/", (req, res) => {
+  res.send("App working");
+});
+
+// Using Error Middleware
+app.use(errorMiddleware);
