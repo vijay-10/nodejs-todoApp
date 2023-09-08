4b31bfc72796173e45ce3ce275228b1aef5f42c1
vijay-10 <vijayvardhansn10@gmail.com>
2023-09-08 14:28:20 +0530

    fixed engine

--- index.js
+++ index.js
@@ -10,37 +10,34 @@ connectDB();
 
 export const app = express();
 
-
-
-app.listen(process.env.PORT, () => {
-  console.log(
-    `Server is working on port:${process.env.PORT} in ${process.env.NODE_ENV} Mode`
-  );
-});
-
-
 config({
-  path: "./.env",
+    path: "./.env",
 });
 
 // Using Middlewares
 app.use(express.json());
 app.use(cookieParser());
 app.use(
-  cors({
-    origin: [process.env.FRONTEND_URL],
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true,
-  })
-);
-
-// Using routes
+    cors({
+        origin: [process.env.FRONTEND_URL],
+        methods: ["GET", "POST", "PUT", "DELETE"],
+        credentials: true,
+    })
+    );
+    
+    // Using routes
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/task", taskRouter);
 
 app.get("/", (req, res) => {
-  res.send("App working");
+    res.send("App working");
 });
 
 // Using Error Middleware
 app.use(errorMiddleware);
+
+app.listen(process.env.PORT, () => {
+  console.log(
+    `Server is working on port:${process.env.PORT} in ${process.env.NODE_ENV} Mode`
+  );
+});
